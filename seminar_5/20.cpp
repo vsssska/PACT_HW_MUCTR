798fa5b0045363dc6632e21e5713df45dd3b608d
Vsssska <babkin60@yandex.ru>
2023-10-04 23:50:50 +0300

    add seminar 5

---
+++ seminar_5/20.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const double PI = 3.141;
+const double R = 8.314;
+const double ALPHA = 1.1;
+const double g = 9.81;
+
+const double t = 60; // Температура в °C
+const double m = 300.0 * 1000 / 3600; // Массовый расход жидкости кг/с
+const double d_outer = 20 * pow(10, -3); // Диаметр внутренний в метрах
+const double delta = 2 * pow(10, -3); // Диаметр внешний в метрах
+const double L = 6; // Длинна труб в метрах
+const double N = 1658; // Число труб
+const double k = 2; // Число ходов теплообменника по трубному пространству
+
+// Вода, незначительная коррозия
+const double e = 0.2 * pow(10, -3); // Абсолютная шероховатость
+const double p = 983.2; // Плотность
+const double mu =  0.4668 * pow(10, -3); // Вязкость
+
+// Кожухотрубный комментарий к-ф сопротивления
+const double E_enter = 1.5;
+const double E_enter_tube = 1.0;
+const double E_exit_tube = 1.0;
+const double E_turn = 2.5;
+const double E_exit = 1.5;
+
+// Объёмный расход жидкости
+double get_v() {
+    return m / p;
+}
+
+// Эквивалентный диаметр трубы круглого сечения
+double get_d_eq() {
+    return d_outer - 2 * delta;
+}
+
+// Площадь сечения трубного пространства теплообменника
+double get_section() {
+    return (N / k) * PI * pow(get_d_eq(), 2) / 4;
+}
+
+// Скорость жидкости
+double get_velocity() {
+    return get_v() / get_section();
+}
+
+// Критерий Рейнольдса
+double get_Re() {
+    return get_velocity() * get_d_eq() * p / mu;
+}
+
+// Относительная шероховатость
+double get_E() {
+    return e / get_d_eq();
+}
+
+// Коэффициент гидравлического трения (коэффициент Дарси)
+double get_Darcy_factor() {
+    return pow(2 * log10((get_E() / 3.7) + pow(6.81 / get_Re(), 0.9)), -2);
+}
+
+// Потери давления на трение
+double get_delta_P_lose() {
+    return get_Darcy_factor() * L * k * p * pow(get_velocity(), 2) / (get_d_eq() * 2);
+}
+
+// Местные сопротивления кожухотрубчатого теплообменника
+double get_E_sum() {
+    return E_enter + E_enter_tube * k + E_exit_tube * k + E_turn * (k - 1) + E_exit;
+}
+
+// Потери давления на местные сопротивления
+double get_delta_P_velocity() {
+    return get_E_sum() * (p * pow(get_velocity(), 2) / 2);
+}
+
+// Общие потери давления
+double get_delta_P_general() {
+    return get_delta_P_lose() + get_delta_P_velocity();
+}
+
+double get_h_lose() {
+    return get_delta_P_general() / (p * g);
+}
+
+int main() {
+    cout << get_h_lose();
+
+    return 0;
+}
---
+++ seminar_5/21.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const double PI = 3.141;
+const double R = 8.314;
+const double ALPHA = 1.1;
+const double g = 9.81;
+
+const double t = 20; // Температура в °C
+const double velocity = 1; // Скорость
+const double D = 1; // Диаметр витка
+const double n = 10; // Число витков
+const double h = 0.1; // Шаг витка
+const double d_outer = 50 * pow(10, -3); // Диаметр внутренний в метрах
+const double delta = 2.5 * pow(10, -3); // Диаметр внешний в метрах
+const double delta_P1 = 1.5 * 98066.5; // Избыточное давление
+
+// Вода, незначительная коррозия
+const double e = 0.2 * pow(10, -3); // Абсолютная шероховатость
+const double p = 998.2; // Плотность
+const double mu =  1.0026 * pow(10, -3); // Вязкость
+
+const double E_enter = 0.2;
+
+// Высота змеевика
+double get_H() {
+    return n * h;
+}
+
+// Длина змеевика
+double get_L() {
+    return n * sqrt(pow((PI * D), 2) + pow(h, 2));
+}
+
+// Эквивалентный диаметр трубы круглого сечения
+double get_d_eq() {
+    return d_outer - 2 * delta;
+}
+
+// Критерий Рейнольдса
+double get_Re() {
+    return velocity * get_d_eq() * p / mu;
+}
+
+// Относительная шероховатость
+double get_E() {
+    return e / get_d_eq();
+}
+
+// Коэффициент гидравлического трения (коэффициент Дарси) для прямой трубы
+double get_Darcy_factor() {
+    return pow(2 * log10((get_E() / 3.7) + pow(6.81 / get_Re(), 0.9)), -2);
+}
+
+//Коэффициент гидравлического трения (коэффициент Дарси) для змеевика
+double get_Darcy_factor_coil() {
+    return get_Darcy_factor() * (1 + 3.54 * get_d_eq() / D);
+}
+
+double get_delta_P_general() {
+    return (ALPHA + get_Darcy_factor_coil() * get_L() / get_d_eq() + E_enter) * p * pow(velocity, 2) / 2;
+}
+
+double get_delta_P_H() {
+    return p * g * get_H();
+}
+
+double get_delta_P2() {
+    return delta_P1 - get_delta_P_H() - get_delta_P_general();
+}
+
+int main() {
+    cout << get_delta_P2() * 0.00001019716212978;
+
+    return 0;
+}
